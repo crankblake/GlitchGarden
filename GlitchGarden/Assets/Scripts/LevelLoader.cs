e32a91ead4b91eb540b31e54646c3b9cae06d97d
EZ3Z <30964083+EZ3Z@users.noreply.github.com>
2019-01-30 23:07:06 -0700

    splash, loading screen, aspect ratio

---
+++ GlitchGarden/Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    [SerializeField] int delayInSeconds = 3;
+    int currentSceneIndex;
+    public void LoadNextScene()
+    {
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+    private IEnumerator WaitForTime()
+    {
+        yield return new WaitForSeconds(delayInSeconds);
+        //SceneManager.LoadScene("StartScreen");
+        LoadNextScene();
+    }
+    private void Start()
+    {
+        currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        if (currentSceneIndex == 0)
+        {
+            StartCoroutine(WaitForTime());
+        }
+    }
+}
---
+++ GlitchGarden/Assets/Scripts/Loading.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Loading : MonoBehaviour
+{
+    [SerializeField] AudioClip loadingSound;
+    [SerializeField] [Range(0, 1)] float loadingSoundVolume = 0.5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        AudioSource.PlayClipAtPoint(loadingSound, Camera.main.transform.position, loadingSoundVolume);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
