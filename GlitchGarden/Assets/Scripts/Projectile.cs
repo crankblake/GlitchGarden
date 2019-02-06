04019389adc0380ca138e4e8d779b54f967d2f2b
EZ3Z <30964083+EZ3Z@users.noreply.github.com>
2019-02-05 19:57:54 -0700

    Projectiles!

---
+++ GlitchGarden/Assets/Scripts/Projectile.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    //[SerializeField] float speedOfSpin = 200f;
+    [SerializeField] float projectileSpeed = 1f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveProjectile();
+    }
+    private void MoveProjectile()
+    {
+      transform.Translate(Vector2.right * Time.deltaTime * projectileSpeed);
+    }
+}
---
+++ GlitchGarden/Assets/Scripts/Shooter.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooter : MonoBehaviour
+{
+    [SerializeField] GameObject projectile;
+    //[SerializeField] GameObject gun;
+    GameObject gun;
+
+    public void Fire()
+    {
+        gun = transform.Find("Gun").gameObject;
+        var newProjectile = Instantiate(projectile, gun.transform.position, Quaternion.identity);
+    }
+    public GameObject GetProjectile()
+    {
+        return projectile;
+    }
+}
