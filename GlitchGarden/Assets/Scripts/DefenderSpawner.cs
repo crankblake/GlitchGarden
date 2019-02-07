29055082aa63438d211dc92232b5266351c7abf0
EZ3Z <30964083+EZ3Z@users.noreply.github.com>
2019-02-06 22:26:11 -0700

    SnapToGrid spawning

--- GlitchGarden/Assets/Scripts/DefenderSpawner.cs
+++ GlitchGarden/Assets/Scripts/DefenderSpawner.cs
@@ -8,12 +8,29 @@ public class DefenderSpawner : MonoBehaviour
     private void OnMouseDown()
     {
         Debug.Log("Mouse was clicked");
-        SpawnDefender();
+        //SnapToGrid(GetSquareClicked());
+        SpawnDefender(GetSquareClicked());
+    }
+    
+    private Vector2 GetSquareClicked()
+    {
+        Vector2 clickPos = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+        Vector2 worldPos = Camera.main.ScreenToWorldPoint(clickPos);
+        //Debug.Log("GetSquareClicked worldPos: " + worldPos);
+        Vector2 gridPos = SnapToGrid(worldPos);
+        //Debug.Log("SnapToGrid gridPos: " + gridPos);
+        return gridPos;
     }
 
-    private void SpawnDefender()
+    private Vector2 SnapToGrid(Vector2 rawWorldPos)
     {
-        GameObject newDefender = Instantiate(defender, transform.position, Quaternion.identity) as GameObject;
+        float newX = Mathf.RoundToInt(rawWorldPos.x);
+        float newY = Mathf.RoundToInt(rawWorldPos.y);
+        return new Vector2(newX, newY);
+    }
+    private void SpawnDefender(Vector2 roundedPause)
+    {
+        GameObject newDefender = Instantiate(defender, roundedPause, Quaternion.identity) as GameObject;
         //Debug.Log(Time.timeSinceLevelLoad);
     }
 }
